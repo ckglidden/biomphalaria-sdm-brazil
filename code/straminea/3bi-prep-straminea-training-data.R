ef8bed3b1e0535c81749a2fc74bf845ce49fb35a
ckglidden <44818401+ckglidden@users.noreply.github.com>
2023-11-05 20:59:47 -0800

    Update 3bi-prep-straminea-training-data.R

--- code/straminea/3bi-prep-straminea-training-data.R
+++ code/straminea/3bi-prep-straminea-training-data.R
@@ -1,4 +1,4 @@
-## CODE TO SET UP DATA FOR FINAL SCHISTO ANALYSIS FOR STRAMINEA
+## code to clean training and validation data for B. straminea model (2000-2017 data)
 
 ##-------------------------------------------##
 ## set wd & load libraries                   ##
